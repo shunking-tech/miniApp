13c2ce43531d148c5a122ba88ba284d6f69cd8c3
shunking-tech <shnktech@gmail.com>
2019-07-14 02:39:52 +0900

    create function article delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,14 @@ class ArticlesController < ApplicationController
     redirect_to :root
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      article.destroy
+      redirect_to :root
+    end
+  end
+
   # private
   #   def article_params
   #     params.permit(:text)
